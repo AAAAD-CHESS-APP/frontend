4af6afe693d751ada23ac236781fef59808a8ccc
Arpan <145697806+arpanmehta05@users.noreply.github.com>
2025-02-06 16:18:46 +0530

    conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import LandingHome from "./components/LandingHome";
-import Navbar from "./components/Navbar";
+import React from "react";
+import Full from "./landing/Full";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Loginpage from "./Login/Loginpage";
+import Signup from "./Login/Signup";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <LandingHome />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Full />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
