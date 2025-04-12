e5476e9f34e19a600891bc91b6c002e7811a00f5
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2025-04-12 17:00:25 +0530

    fix issue : still in waiting queue

--- src/Components/OnlineGame.jsx
+++ src/Components/OnlineGame.jsx
@@ -372,6 +372,11 @@ function OnlineGame() {
         ),
       };
 
+      function cancelSearchAndReturnToDashBoard(){
+        stopSearchingForThisUser();
+        navigate("/dashboard-pannel")
+      }
+
     return (
         <div>
             {RoomName &&
@@ -528,7 +533,7 @@ function OnlineGame() {
                                     Cancel Search
                                 </button>
                                 <button 
-                                    onClick={() => navigate('/dashboard-pannel')} 
+                                    onClick={cancelSearchAndReturnToDashBoard} 
                                     className="w-full border border-gray-300 text-gray-700 py-3 rounded-md hover:bg-gray-50 active:scale-98 transition-all flex items-center justify-center gap-2"
                                 >
                                     <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
